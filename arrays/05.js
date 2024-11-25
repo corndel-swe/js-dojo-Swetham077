05491d98a0763fb29a5e2605c06c5c046f930d2e
Swetha <swetha.muralitharan@axians.co.uk>
2024-11-25 14:45:05 +0000

    fixes arrays05.js

--- arrays/05.js
+++ arrays/05.js
@@ -7,6 +7,9 @@
  */
 export function firstAndLastName(names) {
   // TODO: Get the first element from the array
+  const firstelement = names[0]
   // TODO: Get the last element from the array
+  const lastelement = names[names.length -1]
   // TODO: Return an array containing the first and last elements
+  return [firstelement, lastelement]
 }
